eda02008ae5261e31977a453d69849b080cc6da1
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-16 12:53:12 +0300

    Add project files.

---
+++ Pratik1/Program.cs
@@ -0,0 +1,4 @@
+﻿string fullName = "ahmet sarikaya";
+string myGoal = "1) SQL'deki eksiklerimi tamamlamak\r\n2) Microservice mimarisi hakkında bilgi edinmek\r\n3) React öğrenmek";
+Console.WriteLine($"{fullName}\r\n{myGoal}");
+Console.ReadLine();
