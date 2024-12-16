3bce96653611bc2526d65dd1889409f36c3a0a48
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-16 12:57:55 +0300

    comments

--- Pratik1/Program.cs
+++ Pratik1/Program.cs
@@ -1,4 +1,4 @@
-﻿string fullName = "ahmet sarikaya";
-string myGoal = "1) SQL'deki eksiklerimi tamamlamak\r\n2) Microservice mimarisi hakkında bilgi edinmek\r\n3) React öğrenmek";
-Console.WriteLine($"{fullName}\r\n{myGoal}");
+﻿string fullName = "ahmet sarikaya"; // declare fullName
+string myGoal = "1) SQL'deki eksiklerimi tamamlamak\r\n2) Microservice mimarisi hakkında bilgi edinmek\r\n3) React öğrenmek"; // declare goals
+Console.WriteLine($"{fullName}\r\n{myGoal}"); // output fullName and goals using string interpolation
 Console.ReadLine();
